93a7bdd7706a946529021867853eaf2147ead5aa
Ben Herbert <50344850+ben22020@users.noreply.github.com>
2021-12-21 16:16:20 -0500

    Code Cleanup

--- Project0.Program/Program.cs
+++ Project0.Program/Program.cs
@@ -1,40 +1,114 @@
-﻿namespace Project0.Program{
-    
+﻿namespace Project0.Program {
+
     using System.Data.SqlClient;
     using Project0.DB;
-   
-    
-    public class Program{
-
-        public static string connectionString = File.ReadAllText("../../ben-db-connection-string.txt");
-        public static void Main(string[] args){
 
 
+    public class Program {
 
+       public static string connectionString = File.ReadAllText("../../ben-db-connection-string.txt");
 
+       public static void Main(string[] args)
+        {
             DBInteraction cmd = new DBInteraction(connectionString);
 
-            /*cmd.AddNewCustomer("Bahama", "LLL");
+            cmd.getOrderDetails(12);
+        }
 
-            cmd.findCustomer("Bahama", "LLL");
 
+
+        
+        
+
+        public static void AddCustomerConsole(DBInteraction cmd)
+        {
+            string? firstName;
+            string? lastName;
+            Console.WriteLine("Enter customer first name");
+
+            firstName = Console.ReadLine();
+
+            Console.WriteLine("Enter customer last name");
+
+            lastName = Console.ReadLine();
+
+
+            cmd.AddNewCustomer(firstName ?? "INVALID ENTRY", lastName ?? "INVALID ENTRY");
+
+            Console.WriteLine($"{firstName} {lastName} added to database");
             
-*/
-            //cmd.AddNewLocation("Lucharitos");
-
-           // cmd.placeOrder("1", "1", DateTime.Now, "1", 2);
-            //cmd.listOrderDetailsOfLocation(1);
-
-            cmd.productCatalogue();
-            
-                
-                
-                
-                 
             
         }
 
-      
+        public static void AddLocationConsole(DBInteraction cmd)
+        {
+            string? storeName;
+            
+            Console.WriteLine("Enter Location Name");
+
+            storeName = Console.ReadLine();
+
+            
+            cmd.AddNewLocation(storeName ?? "INVALID ENTRY");
+
+            Console.WriteLine($"{storeName} added to database");
+
+
+        }
+
+        public static void PlaceOrderConsole(DBInteraction cmd)
+        {
+
+            Console.WriteLine("Enter Customer ID: ");
+            string? customerID = Console.ReadLine();
+            Console.WriteLine("Enter location ID: ");
+            string? locationID = Console.ReadLine();
+            Console.WriteLine("Enter Product ID: ");
+            string? productID = Console.ReadLine();
+            Console.WriteLine("Enter Quantity");
+            int quantity = Convert.ToInt32(Console.ReadLine());
+
+            cmd.placeOrder(customerID, locationID, DateTime.Now, productID, quantity);
+        }
+
+        public static void ListLocationOrderConsole(DBInteraction cmd)
+        {
+
+            Console.WriteLine("Enter the ID of the Location you would like to view the order history of");
+            int locID = Convert.ToInt32(Console.ReadLine());
+
+            cmd.listOrderDetailsOfLocation(locID);
+        }
+
+        public static void ListCustomerOrderConsole(DBInteraction cmd)
+        {
+
+            Console.WriteLine("Enter the ID of the Customer you would like to view the order history of");
+            int custID = Convert.ToInt32(Console.ReadLine());
+
+            cmd.listOrderDetailsOfCustomer(custID);
+        }
+
+        public static void ListOrderDetailsConsole(DBInteraction cmd)
+        {
+            Console.WriteLine("Enter the OrderID of the order");
+            int orderId = Convert.ToInt32(Console.ReadLine());
+
+            cmd.getOrderDetails(orderId);
+        }
+
+        public static void FindCustomerConsole(DBInteraction cmd)
+        {
+            Console.WriteLine("Enter the first name followed by the last name");
+
+            string? firstName = Console.ReadLine();  
+            string? lastName = Console.ReadLine();
+
+            cmd.findCustomer(firstName, lastName);
+        }
+
+
+
     }
     
 
