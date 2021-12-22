37fb3e0279688fa61ae99bfb6b37f93ce45a1120
Ben Herbert <50344850+ben22020@users.noreply.github.com>
2021-12-21 21:51:15 -0500

    I

--- Project0.DB/IDBCommands.cs
+++ Project0.DB/IDBCommands.cs
@@ -5,6 +5,8 @@ namespace Project0.DB{
         void AddNewCustomer(string firstName, string lastName);
         void AddNewLocation(string storeName);
 
+        void placeOrder(string customerID, string locationID, DateTime date, string productID, int quantity);
+
 
         
     }
